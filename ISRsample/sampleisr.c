bec73414b67a86b26a15f8a0a1e3f54d616b3b9a
Jeyaraman Rangasamy <jeyaraman.rangasamy@lge.com>
2014-05-15 17:09:01 +0530

    Added sample for ISR register and unregister.
    In this example, IRQ line 21 is considered because it is
    shared between different devices.
    
    Signed-off-by: Jeyaraman R <rjayavrp2@gmail.com>

---
+++ ISRsample/sampleisr.c
@@ -0,0 +1,37 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/interrupt.h>
+
+MODULE_LICENSE("GPL");
+int flag = 1;
+int tempdev = 0;
+
+irqreturn_t sampleISR(int irq, void *dev_id)
+{
+	printk(KERN_ALERT "\n My ISR Called. \n");
+
+	/* To allow actual device to handle it's interrupt. */
+	return IRQ_NONE;		
+}
+
+int __init ourinitmodule(void)
+{
+	printk(KERN_ALERT "\n Welcome to sample application.... \n");
+
+	if(request_irq(21, sampleISR, IRQF_SHARED, KBUILD_MODNAME, (void*)&tempdev))
+	{
+			printk("\n Error in isr registration. \n");
+			flag = 0;	
+	}
+	return 0;
+}
+
+void __exit ourcleanupmodule(void)
+{
+	if(flag)
+		free_irq(21, (void*)&tempdev);
+	printk(KERN_ALERT "\n Thanks....Exiting Application. \n");
+}
+
+module_init(ourinitmodule);
+module_exit(ourcleanupmodule);
